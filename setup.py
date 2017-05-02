81e3daae3f5b7846e837dde1e8ace4424c9a9f9c
GUERIN François <fguerin@ville-tourcoing.fr>
2017-05-02 14:23:18 +0200

    #96 Cartographie des services + Initial integration...

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from __future__ import unicode_literals
 from distutils.core import setup
 
 setup(
-    name='django-buttons',
-    packages=['django-buttons', ],
+    name='wagtail-leaflet',
+    packages=['wagtail-leaflet', ],
     version='0.1',
     description='An application providing some template tags to add buttons to pages',
     author='François GUÉRIN',
     author_email='fguerin@ville-tourcoing.fr',
     url='https://github.com/frague59/django-buttons',
     download_url='https://github.com/frague59/django-buttons/tarball/0.1',
-    keywords=['django', 'django-fontawesome', 'django-appconf'],
-    classifiers=[], requires=['django']
+    keywords=['django', 'wagtail', 'leaflet'],
+    classifiers=[],
+    requires=['wagtail>=1.9',
+              'django-leaflet',
+              'django-geojson', ]
 )
--- wagtailleaflet/__init__.py
+++ wagtailleaflet/__init__.py
@@ -1,2 +1,4 @@
+from __future__ import unicode_literals
+
 #: default app config definition
 default_app_config = 'wagtailleaflet.apps.WagtailLeafletAppConfig'
