d40f37200c13d562f6d6ab1daf0a28b28e328023
GUERIN François <fguerin@ville-tourcoing.fr>
2017-05-02 15:29:28 +0200

    #96 Cartographie des services + Initial integration...

--- wagtailleaflet/blocks.py
+++ wagtailleaflet/blocks.py
@@ -22,18 +22,38 @@ __author__ = 'fguerin'
 logger = logging.getLogger('wagtailleaflet.blocks')
 
 
-class GeoJSONBlock(FieldBlock):
+class GeoJSONPointBlock(FieldBlock):
     """
-    A Geo JSON field in a block !
+    A Geo JSON field into a block !
+    
+    .. note::
+       ``POINT`` item 
     """
     def __init__(self, required=True, help_text=None, max_length=None, min_length=None, **kwargs):
         self.field = GeoJSONFormField(required=required,
                                       help_text=help_text,
                                       max_length=max_length,
                                       min_length=min_length,
-                                      geom_type='Point'
+                                      geom_type='POINT'
                                       )
-        super(GeoJSONBlock, self).__init__(**kwargs)
+        super(GeoJSONPointBlock, self).__init__(**kwargs)
+
+
+class GeoJSONMultiPointBlock(FieldBlock):
+    """
+    A Geo JSON field into a block !
+    
+    .. note::
+       ``MultiPoint`` item 
+    """
+    def __init__(self, required=True, help_text=None, max_length=None, min_length=None, **kwargs):
+        self.field = GeoJSONFormField(required=required,
+                                      help_text=help_text,
+                                      max_length=max_length,
+                                      min_length=min_length,
+                                      geom_type='MULTIPOINT'
+                                      )
+        super(GeoJSONMultiPointBlock, self).__init__(**kwargs)
 
 
 class LeafletBlock(StructBlock):
@@ -42,15 +62,41 @@ class LeafletBlock(StructBlock):
     """
     title = CharBlock()
 
-    geojson = GeoJSONBlock()
+    location = GeoJSONPointBlock()
 
     def render_form(self, value, prefix='', errors=None):
+        """
+        Renders ``edit`` form
+        
+        :param value: current value 
+        :param prefix: prefix of the form item 
+        :param errors: Validations errors
+        :returns: HTML Fragment  
+        """
         logger.debug('LeafletBlock::render_form() value = %s', value)
         rendered = super(LeafletBlock, self).render_form(value=value, prefix=prefix, errors=errors)
         return rendered
 
+    def render(self, value, context=None):
+        """
+        Renders the widget in the web site
+        
+        :param value: current value 
+        :param context: Additional render context 
+        :returns: HTML Fragment  
+        """
+        logger.debug('LeafletBlock::render() value = %s', value)
+        rendered = super(LeafletBlock, self).render(value=value, context=context)
+        return rendered
+
     def html_declarations(self):
         output = render_to_string('wagtailleaflet/leaflet_forms.html')
+        # logger.debug('LeafletBlock::html_declarations() output = %s', output)
+        return output
+
+    def js_initializer(self):
+        output = 'drawMap'
+        logger.debug('LeafletBlock::js_initializer() output = %s', output)
         return output
 
     class Meta:
@@ -59,4 +105,4 @@ class LeafletBlock(StructBlock):
         label = _('Map')
 
     class Media:
-        js = static('wagtailleaflet/leaflet.js')
+        js = static('wagtailleaflet/leaflet_init.js')
---
+++ wagtailleaflet/static/wagtailleaflet/leaflet_init.js
@@ -0,0 +1,8 @@
+function drawMap(definitionPrefix) {
+    var newItemHtml = $('#' + definitionPrefix + '-location').html();
+
+    var initMap = function(definitionPrefix) {
+        // pass
+    };
+    return initMap;
+}
